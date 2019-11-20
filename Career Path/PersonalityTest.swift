c26ac0473ff25386030d23c24c0f768ceebabb60
katja <ekaterina.seikkinen@gmail.com>
2019-11-20 20:25:17 +0200

    new commit

---
+++ Career Path/PersonalityTest.swift
@@ -0,0 +1,9 @@
+//
+//  PersonalityTest.swift
+//  Career Path
+//
+//  Created by iosdev on 20/11/2019.
+//  Copyright © 2019 Mikael Kuokkanen. All rights reserved.
+//
+
+import Foundation
---
+++ Career Path/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  Career Path
+//
+//  Created by iosdev on 20/11/2019.
+//  Copyright © 2019 Mikael Kuokkanen. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
