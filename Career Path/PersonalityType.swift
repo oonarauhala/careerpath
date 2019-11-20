09896b2335597d5fae4a7f1e3102b0b0aee8eba1
mikael <theonewhoknocks420abq@wtf.com>
2019-11-20 14:50:52 +0200

    forgot to include PersonalityType

---
+++ Career Path/PersonalityType.swift
@@ -0,0 +1,27 @@
+//
+//  PersonalityType.swift
+//  CareerPath
+//
+//  Created by iosdev on 19/11/2019.
+//  Copyright © 2019 Mikael Kuokkanen. All rights reserved.
+//
+
+enum PersonalityType : String {
+    case
+    ISTJ = "Introverted, Sensing, Thinking, Judging",
+    ISFJ = "Introverted, Sensing, Feeling, Judging",
+    INFJ = "Introverted, Intuitive, Feeling, Judging",
+    INTJ = "Introverted, Intuitive, Thinking Judging",
+    ISTP = "Introverted, Sensing, Thinking, Perceiving",
+    ISFP = "Introverted, Sensing, Feeling, Perceiving",
+    INFP = "Introverted, Intuitive, Feeling, Perceiving",
+    INTP = "Introverted, Intuitive, Thinking, Perceiving",
+    ESTP = "Extraverted, Sensing, Thinking, Perceiving",
+    ESFP = "Extraverted, Sensing, Feeling, Perceiving",
+    ENFP = "Extraverted, Intuitive, Feeling, Perceiving",
+    ENTP = "Extraverted, Intuitive, Thinking, Perceiving",
+    ESTJ = "Extraverted, Sensing, Thinking, Judging",
+    ESFJ = "Extraverted, Sensing, Feeling, Judging",
+    ENFJ = "Extraverted, Intuitive, Feeling, Judging",
+    ENTJ = "Extraverted, Intuitive, Thinking, Judging"
+}
