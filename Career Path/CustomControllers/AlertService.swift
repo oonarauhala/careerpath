b404d067d830e415b672680b7c47cac85ff44f20
oonarauhala <oona.e.rauhala@gmail.com>
2019-11-24 17:56:26 +0200

    Create customAlert storyboard, alertController, alertService

---
+++ Career Path/CustomControllers/AlertService.swift
@@ -0,0 +1,24 @@
+//
+//  AlertService.swift
+//  Career Path
+//
+//  Created by Oona on 24/11/2019.
+//  Copyright © 2019 Team Awesome. All rights reserved.
+//
+
+// This file includes the alert function
+
+import UIKit
+
+class AlertService {
+    
+    func alert() -> AlertViewController {
+        
+        // don't know if bundle is righr??
+        let storyboard = UIStoryboard(name: "Alert", bundle: .main)
+        
+        let alertVC = storyboard.instantiateViewController(withIdentifier: "AlertVC") as! AlertViewController
+        
+        return alertVC
+    }
+}
