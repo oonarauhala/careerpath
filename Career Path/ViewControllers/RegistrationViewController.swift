aac2c4589970b8bf1bf24b1821d132a0ffc1de91
oonarauhala <oona.e.rauhala@gmail.com>
2019-12-03 21:04:33 +0200

    Use basic validation

--- Career Path/ViewControllers/RegistrationViewController.swift
+++ Career Path/ViewControllers/RegistrationViewController.swift
@@ -9,7 +9,55 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+
+    var validFields = 0
     
+    // Outlets for all fields
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
     
+    //actions for all fields
+    @IBAction func usernameEditingChanged(_ sender: UITextField) {
+        guard let username = usernameField.text else {
+            return
+        }
+        if username.isValid(.username) {
+            print("Username is valid")
+            
+            //set username to core data/database HERE!!
+            
+            validFields += 1
+            
+            //if all fields are valid, enable button
+            if validFields == 4 {
+                //enable button
+            }
+        }
+        else {
+            //show user their username was not valid
+            print("Not a valid username")
+        }
+    }
     
+    @IBAction func emailEditingChanged(_ sender: UITextField) {
+        guard let email = emailField.text else {
+            return
+        }
+        if email.isValid(.email) {
+            print("Email is valid")
+            
+            //set email to core data/database HERE!!
+            
+            validFields += 1
+            
+            //if all fields are valid, enable button
+            if validFields == 4 {
+                //enable button
+            }
+        }
+        else {
+            //show user email was not valid
+            print("Not a valid email")
+        }
+    }
 }
